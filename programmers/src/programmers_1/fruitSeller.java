32e672fab3b8d9596e6090f3cbeffbb3be5d2cd7
user1 <user1@host.docker.internal>
2022-11-16 13:39:38 +0900

    Lv.1 과일장수

---
+++ programmers/src/programmers_1/fruitSeller.java
@@ -0,0 +1,35 @@
+package programmers_1;
+
+import java.util.*;
+
+public class fruitSeller {
+
+	public static void main(String[] args) {
+		
+		int k = 3;
+		int m = 4;
+		int[] score = { 1, 2, 3, 1, 2, 3, 1 };
+		
+		int answer = 0;
+		int n = score.length - 1;
+		
+		int[][] apple = new int[score.length/m][m];
+		
+		Arrays.sort(score);
+		
+		for(int i = 0; i < score.length / m; i++) {
+			for(int j = 0; j < m; j++) {
+				apple[i][j] = score[n];
+				n--;
+			}
+		}
+		for(int i = 0; i < apple.length; i++) {
+			for(int j = 0; j < apple[0].length; j++) {
+				if(j == apple[0].length - 1) {
+					answer += apple[i][j] * m; 
+				}
+			}
+		}
+	}
+
+}
