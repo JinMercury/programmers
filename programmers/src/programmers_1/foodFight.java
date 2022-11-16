41246ea61747b768b5293ef56ae1b65344adf6cc
user1 <user1@host.docker.internal>
2022-11-16 13:39:48 +0900

    Lv.1 푸드 파이트 대회

---
+++ programmers/src/programmers_1/foodFight.java
@@ -0,0 +1,25 @@
+package programmers_1;
+
+public class foodFight {
+
+	public static void main(String[] args) {
+		
+		int[] food = {1, 3, 4, 6};
+		String answer = "";
+		
+		for(int i = 1; i < food.length; i++) {
+			for(int j = 0; j < food[i] / 2; j++) {
+				answer = answer.concat(String.valueOf(i));
+			}
+		}
+		answer = answer.concat("0");
+		for(int i = food.length - 1; i > 0; i--) {
+			for(int j = 0; j < food[i] / 2; j++) {
+				answer = answer.concat(String.valueOf(i));
+			}
+		}
+        System.out.println(answer);
+
+	}
+
+}
