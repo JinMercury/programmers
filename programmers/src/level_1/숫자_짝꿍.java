5320778fe3acf8dffd154b75c09254d6882af36f
user1 <user1@host.docker.internal>
2022-11-21 10:26:40 +0900

    level 1 문제풀이

--- programmers/src/level_1/숫자_짝꿍.java
+++ programmers/src/level_1/숫자_짝꿍.java
@@ -3,13 +3,12 @@ package level_1;
 import java.util.*;
 
 public class 숫자_짝꿍 {
-
-	public static void main(String[] args) {
-		
-		String X = "12321"; String Y = "42531";
-		
+	
+	public static String solution(String X, String Y) {
 		int[] xArr = new int[10];
 		int[] yArr = new int[10];
+		StringBuilder answer = new StringBuilder(1024);
+		answer.setLength(0);
 		String result = "";
 		
 		for(int i = 0; i < X.length(); i++) {
@@ -26,18 +25,24 @@ public class 숫자_짝꿍 {
 			} else {
 				temp = xArr[i];
 			}
-			if(i == 0 && result.equals("") && temp > 0) {
-				System.out.println(0);
+			if(i == 0 && answer.length() == 0 && temp > 0) {
+				return "0";
 			}
 			
 			for(int j = 0; j < temp; j++) {
-				result += i;
+				answer.append(i);
 			}
 		}
-		if(result.equals("")) {
-			System.out.println(-1);
+		if(answer.length() == 0) {
+			return "-1";
 		}
-		System.out.println(result);
+		result = answer.toString();
+		return result;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(solution("100", "2345"));
 
 	}
 
