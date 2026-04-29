7e4e8c8453a5273ebc12b8c57b1d966d23cd79d3
Mercury <39897996+JinMercury@users.noreply.github.com>
2026-04-29 16:45:32 +0900

    노란불_신호등

---
+++ programmers/src/all_level_1/노란불_신호등.java
@@ -0,0 +1,23 @@
+package all_level_1;
+
+public class 노란불_신호등 {
+
+    public static void main (String[] args){
+        int[][] signals = {{2,1,1}, {5,1,1}};
+
+        System.out.println(Arrays.toString(solution(signals)));
+    }
+
+    private static Int[][] solution(Int answer) {
+        ArrayList<Integer> signals = new ArrayList<>();
+
+        System.out.println(signals.length);
+        /* while(color == 0) {
+            for(i = 0; i < signals.length; i++) {
+
+            }
+        } */
+    
+    }
+}
+
